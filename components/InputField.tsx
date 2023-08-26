50ba6ff60e0dbab7738f268453a9627e3806f597
sunil reddy <sunday7637@gmail.com>
2023-08-26 15:55:32 +0530

    second commit

---
+++ components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <form>
+      <input type="text" placeholder='Enter anything'/>
+      <button>Enter</button>
+    </form>
+  )
+}
+
+export default InputField
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-const Home = () => {
-  return <div>hello sunil </div>;
+import InputField from "../../components/InputField";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col  items-center my-7  gap-5 ">
+      <h1 className="font-bold text-3xl text-black">NoteType</h1>
+      <InputField />
+    </div>
+  );
 };
 
 export default Home;
