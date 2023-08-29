44916aca1b634a01fee7043cbed87d01ca7ab231
sunil reddy <sunday7637@gmail.com>
2023-08-29 19:04:32 +0530

    fixed bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,18 @@ const Home: React.FC = () => {
   const [data, setData] = useState<Todotype[]>([]);
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+    if (todo) {
+      setData([...data, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
   };
 
-  if (todo) {
-    setData([...data, { id: Date.now(), todo, isDone: false }]);
-    setTodo("");
-  }
   // console.log(data);
   return (
     <div className="flex flex-col  items-center my-7  gap-5 ">
       <h1 className="font-bold text-3xl text-black">NoteType</h1>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       {data.map((t) => (
-        
         <h1>{t.todo}</h1>
       ))}
     </div>
